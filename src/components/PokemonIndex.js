4cda647fb638cf3c543cf5fd32ae29dc1b5c132d
Raza <rtshareef@gmail.com>
2020-03-03 15:19:12 -0500

    fetched data

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
+const pokemon_url = "http://localhost:3000/pokemon";
 
-class PokemonPage extends React.Component {
+class PokemonIndex extends React.Component {
+  state = {
+    pokemon: []
+  };
+
+  componentDidMount() {
+    fetch(pokemon_url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ pokemon: data });
+      });
+  }
+
   render() {
+    // console.log(this.state.pokemon);
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
         <PokemonForm />
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+          onSearchChange={_.debounce(() => console.log("🤔"), 500)}
+          showNoResults={false}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonIndex;
