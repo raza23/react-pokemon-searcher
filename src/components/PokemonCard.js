3b4b0d3fec8e89033a96cdd47a13d7293effa2bc
Raza <rtshareef@gmail.com>
2020-03-03 16:08:59 -0500

    cards can be flipped

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,14 +2,25 @@ import React from "react";
 import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    cardFlipped: true
+  };
+
+  handleClick = () => {
+    this.setState({ cardFlipped: !this.state.cardFlipped });
+  };
+
   render() {
     console.log(this.props);
     const hp = this.props.stats.find(stat => stat.name === "hp");
+    const sprite = this.state.cardFlipped
+      ? this.props.sprites.front
+      : this.props.sprites.back;
     return (
       <Card>
-        <div>
+        <div onClick={this.handleClick}>
           <div className="image">
-            <img src={this.props.sprites.front} alt="oh no!" />
+            <img src={sprite} alt="oh no!" />
           </div>
           <div className="content">
             <div className="header">{this.props.name}</div>
