9a51c06b31593cdd08863d6b9f96f4f645bbf49e
Raza <rtshareef@gmail.com>
2020-03-03 15:22:13 -0500

    pokemon cards appearing on DOM

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,10 +4,14 @@ import { Card } from "semantic-ui-react";
 
 class PokemonCollection extends React.Component {
   render() {
-    console.log(this.props);
+    // console.log(this.props);
+    const pokemonCard = this.props.pokemon.map(pokemon => {
+      return <PokemonCard />;
+    });
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {pokemonCard}
       </Card.Group>
     );
   }
