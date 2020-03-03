9399f1b4419d6294d5c00289a8deeff3c555f2c1
Raza <rtshareef@gmail.com>
2020-03-03 16:03:06 -0500

    pokemon sprites showing up

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
   render() {
+    console.log(this.props);
+    const hp = this.props.stats.find(stat => stat.name === "hp");
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.props.sprites.front} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp.value} hp
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -6,11 +6,13 @@ class PokemonCollection extends React.Component {
   render() {
     // console.log(this.props);
     const pokemonCard = this.props.pokemon.map(pokemon => {
-      return <PokemonCard />;
+      return <PokemonCard key={pokemon.id} {...pokemon} />;
     });
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        <h1>
+          <center>Hello From Pokemon Collection</center>
+        </h1>
         {pokemonCard}
       </Card.Group>
     );
